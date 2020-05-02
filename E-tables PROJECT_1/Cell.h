29311ecc68e6133b2803fbb9a6d65b045bb16293
Maria Grozdeva <mggrozdeva@gmail.com>
2020-05-03 00:03:18 +0300

    save and saveas

---
+++ E-tables PROJECT_1/Cell.h
@@ -0,0 +1,10 @@
+template <typename T>
+class Cell
+{
+private:
+	T value;
+	T formula;
+
+public:
+	void edit();
+};
--- E-tables PROJECT_1/E-tables PROJECT_1.cpp
+++ E-tables PROJECT_1/E-tables PROJECT_1.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+void save(String& diffFileName);
+
 int main()
 {
 	char command[8];
@@ -11,7 +13,7 @@ int main()
 	while (strcmp(command, "exit") != 0)
 	{
 		fstream originalFile;
-		ofstream copyOFile;
+		ofstream copyFile;
 
 		String fileName;
 
@@ -31,9 +33,9 @@ int main()
 
 				fileName.push_back(letter);
 			}
-			
+
 			originalFile.open(fileName.getStr());
-			copyOFile.open("temp.txt");
+			copyFile.open("temp.txt");
 
 			if (originalFile.fail())
 			{
@@ -43,30 +45,107 @@ int main()
 
 				originalFile.open(fileName.getStr());
 			}
+			else
+				cout << "Successfully opened" << " " << fileName.getStr();
 
-			while (!originalFile.eof())
+			while (originalFile.get(letter))
 			{
 				String buff;
 
-				originalFile.get(letter);
 				buff.push_back(letter);
 
-				copyOFile << buff.getStr();
+				copyFile << buff.getStr();
 			}
 
 			originalFile.close();
+			copyFile.close();
 
 			while (true)
 			{
 				cin >> command;
+				cin.ignore();
 
 				if (strcmp(command, "close") == 0)
 				{
-					copyOFile.close();
 					cout << "Successfully closed" << " " << fileName.getStr();
 					break;
 				}
+
+				else if (strcmp(command, "exit") == 0)
+				{
+					cout << "Exiting the program..." << endl;
+					return 0;
+				}
+
+				else if (strcmp(command, "help") == 0)
+				{
+					cout << "The following commands are supported:" << endl;
+					cout << "open <file>    opens <file>" << endl;
+					cout << "close			closes currently opened file" << endl;
+					cout << "save			saves the currently open file" << endl;
+					cout << "saveas <file>	saves the currently open file in <file>" << endl;
+					cout << "help			prints this information" << endl;
+				}
+
+				else if (strcmp(command, "save") == 0)
+				{
+					save(fileName);
+
+					cout << "Successfully saved " << fileName.getStr() << endl;
+				}
+
+				else if (strcmp(command, "saveas") == 0)
+				{
+					String userChosenFileName;
+
+					while (true)
+					{
+						cin.get(letter);
+
+						if (letter == ' ' || letter == '\n')
+							break;
+
+						userChosenFileName.push_back(letter);
+					}
+
+					cout << "Successfully saved another " << userChosenFileName.getStr() << endl;
+
+					ofstream userChosenFile;
+					userChosenFile.open(userChosenFileName.getStr());
+
+					userChosenFile.close();
+				
+					save(userChosenFileName);
+				}
+
+				else if (strcmp(command, "edit") == 0);
+
+				else if (strcmp(command, "print") == 0);
+
 			}
 		}
 	}
+}
+
+void save(String& diffFileName)
+{
+	ofstream originalFile;
+	fstream copyFile;
+
+	originalFile.open(diffFileName.getStr());
+	copyFile.open("temp.txt");
+
+	char letter{ '0' };
+
+	while (copyFile.get(letter))
+	{
+		String buff;
+
+		buff.push_back(letter);
+
+		originalFile << buff.getStr();
+	}
+
+	originalFile.close();
+	copyFile.close();
 }
