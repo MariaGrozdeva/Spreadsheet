9a59b0e5172e669c0a3b1502e06919065f6253de
Maria Grozdeva <mggrozdeva@gmail.com>
2020-05-03 16:56:54 +0300

    commands in classes

--- E-tables PROJECT_1/CommandLine.cpp
+++ E-tables PROJECT_1/CommandLine.cpp
@@ -1,1 +1,76 @@
-#include "CommandLine.h"
+﻿#include "CommandLine.h"
+#pragma warning (disable : 4996)
+
+void CommandLine::setCommand()
+{
+	cin >> command;
+	cin.ignore();
+}
+
+void CommandLine::helperForSaveAndSaveas(String& fileName)
+{
+	ofstream originalFile; // ofstream, ще записваме промените в оригиналния файл
+	ifstream copyFile; // ifstream, ще ги четем от файла- копие на оригиналния
+
+	originalFile.open(fileName.getStr());
+	copyFile.open("temp.txt");
+
+	while (copyFile.get(letter))
+	{
+		String buff;
+
+		buff.push_back(letter);
+
+		originalFile << buff.getStr();
+	}
+
+	originalFile.close();
+	copyFile.close();
+}
+
+void CommandLine::save(String& originalFileName)
+{
+	helperForSaveAndSaveas(originalFileName);
+
+	cout << "Successfully saved " << fileName.getStr() << endl;
+}
+
+void CommandLine::saveas()
+{
+	String userChosenFileName; // Създаваме името на файла, в което потребителят иска да запише промените.
+
+	while (true)
+	{
+		cin.get(letter);
+
+		if (letter == ' ' || letter == '\n')
+			break;
+
+		userChosenFileName.push_back(letter);
+	}
+
+	helperForSaveAndSaveas(userChosenFileName);
+
+	cout << "Successfully saved another " << userChosenFileName.getStr() << endl;
+}
+
+void CommandLine::close() const
+{
+	cout << "Successfully closed" << " " << fileName.getStr();
+}
+
+void CommandLine::exit() const
+{
+	cout << "Exiting the program..." << endl;
+}
+
+void CommandLine::help() const
+{
+	cout << "The following commands are supported:" << endl;
+	cout << "open <file>    opens <file>" << endl;
+	cout << "close			closes currently opened file" << endl;
+	cout << "save			saves the currently open file" << endl;
+	cout << "saveas <file>	saves the currently open file in <file>" << endl;
+	cout << "help			prints this information" << endl;
+	cout << "exit			exists the program" << endl;
+}
--- E-tables PROJECT_1/CommandLine.h
+++ E-tables PROJECT_1/CommandLine.h
@@ -1,3 +1,33 @@
+﻿#include <fstream>
+#include "String.h"
+
+using namespace std;
+
+const int MAX_SIZE_OF_COMMAND = 8;
+
 class CommandLine
 {
+protected: // Тук използваме protected атрибут за достъп, защото в класа MainCommand, които ще е производен
+	// за CommandLine, ще искаме да имаме вътрешен достъп до наследените член- данни на базовия клас.
+	String fileName;
+	char command[MAX_SIZE_OF_COMMAND];
+	char letter;
+
+	void helperForSaveAndSaveas(String& diffFileName); // &, за да не създаваме копие на името.
+// Тази функция е декларирана в private секцията, защото ще я ползваме само ние, не искаме потребителят да има
+// достъп до нея. В зависимост от това дали ще я викаме в save или saveas- методите, ще и подаваме съответно
+// originalFileName(записваме промените в оригиналния файл) или userChosenFileName(записваме ги в избран от
+// потребителя такъв).
+
+public:
+	void setCommand();
+
+	void save(String& originalFileName);
+	void saveas();
+	void close() const;
+	void exit() const;
+	void help() const;
+
+	//void print();
+	//void edit();
 };
